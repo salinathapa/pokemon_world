c486f2efb1b670f170571de7152bc29e188bc681
salinathapa <salina.business@hotmail.com>
2023-09-07 10:31:43 +0100

    export pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as HomePage } from './HomePage';
+export { default as SuperherosPage } from './SuperherosPage';
+export { default as SuperheroPage } from './SuperheroPage';
+export { default as NotFoundPage } from './NotFoundPage';
