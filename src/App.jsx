4cf28127cd8b5562bc19798c4e79f5f77ac8b34b
salinathapa <salina.business@hotmail.com>
2023-09-07 10:36:02 +0100

    add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import { HomePage, SuperherosPage, SuperheroPage, NotFoundPage } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/superheros" element={<SuperherosPage />} />
+      <Route path="/superheros/:id" element={<SuperheroPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   )
 }
 
