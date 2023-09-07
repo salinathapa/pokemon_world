edead7004cbbcd560eb2c22d59c4fd5df8941020
salinathapa <salina.business@hotmail.com>
2023-09-07 10:27:58 +0100

    add BrowserRouter to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
