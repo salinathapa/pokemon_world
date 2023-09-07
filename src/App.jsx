ceb135c8070dd3689249c12318cd0499837ab08b
salinathapa <salina.business@hotmail.com>
2023-09-07 13:00:37 +0100

    add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,13 @@ const App = () => {
           <Route path="/pokemon" element={<PokemonGenerationsPage />} />
           <Route path="/kanto" element={<KantoPage />} />
           <Route path="/johto" element={<JohtoPage />} />
+          {/* <Route path="/hoenn" element={<HoennPage />} />
+          <Route path="/sinnoh" element={<SinnohPage />} />
+          <Route path="/unova" element={<UnovaPage />} />
+          <Route path="/kalos" element={<KalosPage />} />
+          <Route path="/alola" element={<AlolaPage />} />
+          <Route path="/galar" element={<GalarPage />} />
+          <Route path="/paldea" element={<PaldeaPage />} /> */}
           <Route path="/pokemon/:id" element={<PokemonPage />} />
           <Route path="/search" element={<SearchPokemonPage />} />
           <Route path="*" element={<NotFoundPage />} />
